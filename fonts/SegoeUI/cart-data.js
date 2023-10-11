72fc70d9ceca7dc5c099e1655004e3ff84fbb28d
Mari Vaypane <avemari98@gmail.com>
2023-10-12 01:47:45 +0300

    add template of cart data

---
+++ fonts/SegoeUI/cart-data.js
@@ -0,0 +1,14 @@
+const cardData = [
+    {
+        id: 143654,
+        itemTitle: 'Футболка UZcotton мужская',
+        picture: '',
+        color: 'Белый',
+        size: '56',
+        wb: 'Коледино WB',
+        shop: 'OOO Вайлдберриз',
+        amount: '',
+        price: '', //one grand price
+        //priceWithDiscount: '', //one grand price
+    }
+]
