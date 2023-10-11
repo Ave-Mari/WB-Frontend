1f56517e578f842486a46c1539e1add2121e314d
Mari Vaypane <avemari98@gmail.com>
2023-10-12 02:15:07 +0300

    move file

---
+++ cart-data.js
@@ -0,0 +1,38 @@
+const cardData = [
+    {
+        id: 143654,
+        itemTitle: 'Футболка UZcotton мужская',
+        picture: '',
+        color: 'Белый',
+        size: '56',
+        wb: 'Коледино WB',
+        shop: 'OOO Вайлдберриз',
+        amount: 1,
+        price: 1051, //one grand price
+        discount: 50.33
+    },
+    {
+        id: 2645656,
+        itemTitle: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
+        picture: '',
+        color: 'Прозрачный',
+        size: '',
+        wb: 'Коледино WB',
+        shop: 'OOO Мегапрофстиль',
+        amount: 200,
+        price: 11500.235, //one grand price
+        discount: '', 
+    },
+    {
+        id: 372850,
+        itemTitle: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber-Castell',
+        picture: '',
+        color: '',
+        size: '',
+        wb: 'Коледино WB',
+        shop: 'OOO Вайлдберриз',
+        amount: 2,
+        price: '', //one grand price
+        discount: '', //one grand price
+    }
+]
