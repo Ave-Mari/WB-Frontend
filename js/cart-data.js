992e0cc7f28df9537de3d4d99a3793f66e3bd298
Mari Vaypane <avemari98@gmail.com>
2023-10-15 19:55:39 +0300

    add classes to cart items in object

--- js/cart-data.js
+++ js/cart-data.js
@@ -1,6 +1,7 @@
 const cartData = [
     {
         id: 143654,
+        class: 'tshirt',
         itemTitle: 'Футболка UZcotton мужская',
         picture: '../styles/cart/cart-data-images/t-shirt.png',
         color: 'Белый',
@@ -13,6 +14,7 @@ const cartData = [
     },
     {
         id: 2645656,
+        class: 'case',
         itemTitle: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
         picture: '../styles/cart/cart-data-images/phone-case.png',
         color: 'Прозрачный',
@@ -21,10 +23,11 @@ const cartData = [
         wb: 'OOO Мегапрофстиль',
         amount: 200,
         price: 11500.235, //one grand price
-        discount: '', 
+        discount: 8.69, 
     },
     {
         id: 372850,
+        class: 'percils',
         itemTitle: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber-Castell',
         picture: '../styles/cart/cart-data-images/pencils.png',
         color: '',
@@ -32,8 +35,8 @@ const cartData = [
         shop: 'Коледино WB',
         wb: 'OOO Вайлдберриз',
         amount: 2,
-        price: '', //one grand price
-        discount: '', //one grand price
+        price: 472, //one grand price
+        discount: 48, //one grand price
     }
 ]
 
