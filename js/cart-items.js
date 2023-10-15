37b22f33eda0c84f7cc155b5ea0db7a766d42b08
Mari Vaypane <avemari98@gmail.com>
2023-10-15 19:56:08 +0300

    add discount and condition showing to elements

--- js/cart-items.js
+++ js/cart-items.js
@@ -4,10 +4,16 @@ const cartList = document.querySelector(".cart-items-list");
 
 export const cartMap = (cartData) => {
   let mapCartItems = cartData.map((item) => {
+    const discountPrice = Math.floor(
+      item.price - (item.discount / 100) * item.price
+    );
     return `
         <li class="cart-item">     
-        <div class="item-wrapper">
-        <input type="checkbox" />
+        <div class="item-wrapper">     
+        <div class="checkbox-wrapper checkbox-cart">
+                      <input type="checkbox" checked id=${item.class} />
+                      <label for=${item.class}></label>                   
+                    </div>
             <div class="picture-wrapper">
                 <img class="item-picture" src=${item.picture} alt=${
       item.itemTitle
@@ -15,20 +21,26 @@ export const cartMap = (cartData) => {
             </div>
             <div class="params-wrapper">
                 <p class="title">${item.itemTitle}</p>
-                <div class="color-and-size-wrapper">
-                ${
-                  item.color
-                    ? `<p class="color">Цвет: <span class="color-dynamic">${item.color}</span></p>`
-                    : ""
-                }
-                  ${
-                    item.size
-                      ? `<p class="size">Размер: <span class="size-dynamic">${item.size}</span></p>`
-                      : ""
-                  }
-                    
-                
-                </div>
+               ${
+                 item.color || item.size
+                   ? `
+               <div class="color-and-size-wrapper">
+${
+  item.color
+    ? `<p class="color">Цвет: <span class="color-dynamic">${item.color}</span></p>`
+    : ""
+}
+  ${
+    item.size
+      ? `<p class="size">Размер: <span class="size-dynamic">${item.size}</span></p>`
+      : ""
+  }
+    
+
+</div>
+               `
+                   : ""
+               }
                 <p class="shop">${item.shop}</p>
                 <p class="wb">${item.wb}</p>
             </div>
@@ -41,10 +53,26 @@ export const cartMap = (cartData) => {
                 <button class="amount-button" id="btn-increment">+</button>      
                 </div>                    
                 <p class="amount-text">Осталось <span class="amount-dynamic">2</span> шт.</p>
+                <div class="icons-wrapper">
+                <button class="button-with-icon"> 
+                <img class="button-icon" src='../styles/cart/like.svg' alt="like">
+                </button>
+                <button class="button-with-icon"> 
+                <img class="button-icon" src='../styles/cart/delete.svg' alt="delete">
+                </button>
+                </div>
             </div>
             <div class="price">
-                <p class="price-text"><span class="price-dynamic">
-                ${item.price * item.amount}</span> сом</p>
+               <p class="price-discount-text ${
+                 discountPrice.toString().length > 3 ? "s-text" : ""
+               }">
+                <span class="price-dynamic">
+                ${(discountPrice * item.amount).toLocaleString()}
+                </span> сом</p>
+                <p class="price-original-text">
+                <span class="price-dynamic">
+                ${(item.price * item.amount).toLocaleString()}
+                </span> сом</p>                
             </div>
         </div>
        
@@ -54,3 +82,4 @@ export const cartMap = (cartData) => {
   mapCartItems = mapCartItems.join("");
   cartList.innerHTML = mapCartItems;
 };
+
