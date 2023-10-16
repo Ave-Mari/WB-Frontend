d8a02c90eb4c88f51306df8146bc5d70a8b20499
Mari Vaypane <avemari98@gmail.com>
2023-10-17 01:39:03 +0300

    import accordion function to main js file

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import { cartMap } from "./js/cart-items.js";
 import cartData from "./js/cart-data.js";
+import { runAccordion } from './js/cart-accordeon.js'
+
 
 cartMap(cartData);
+runAccordion();
+
